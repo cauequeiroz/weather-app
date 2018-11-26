4cacb848289c16f7ce88e0eb00d2d7c35cc7befc
Caue Queiroz <cauenqueiroz@gmail.com>
2018-11-26 20:03:54 -0200

    search for lat and long coords for user input address

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
 
-const url = 'http://www.mapquestapi.com/geocoding/v1/address?key=HWwuSA8PguefUNIfjjjAoUoizls5rzwD&location=1301%20lombard%20street%20philadelphia';
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      string: true,
+      alias: 'address',
+      description: 'An address to find lat and long'
+    }
+  })
+  .help()
+  .alias('h', 'help')
+  .argv;
+
+const encodedAddress = encodeURIComponent(argv.address);
+const url = `http://www.mapquestapi.com/geocoding/v1/address?key=HWwuSA8PguefUNIfjjjAoUoizls5rzwD&location=${encodedAddress}`;
 
 request({ url, json: true }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].providedLocation.location}`);
+  console.log(`Lat: ${body.results[0].locations[0].latLng.lat}`);
+  console.log(`Long: ${body.results[0].locations[0].latLng.lng}`);
 });
